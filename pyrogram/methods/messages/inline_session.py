9f4e84de4395d0c5b89846fa837d27b7f0235bfd
subinps <64341611+subinps@users.noreply.github.com>
2021-11-13 20:52:13 +0530

    try

--- pyrogram/methods/messages/inline_session.py
+++ pyrogram/methods/messages/inline_session.py
@@ -16,22 +16,29 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from asyncio import Lock
+
 import pyrogram
 from pyrogram import raw
 from pyrogram.errors import AuthBytesInvalid
 from pyrogram.session import Session
 from pyrogram.session.auth import Auth
 
+lock = Lock()
+sessions = {}
+
 
 async def get_session(client: "pyrogram.Client", dc_id: int):
     if dc_id == await client.storage.dc_id():
         return client
 
-    async with client.media_sessions_lock:
-        if client.media_sessions.get(dc_id):
-            return client.media_sessions[dc_id]
+    async with lock:
+        global sessions
 
-        session = client.media_sessions[dc_id] = Session(
+        if sessions.get(dc_id):
+            return sessions[dc_id]
+
+        session = sessions[dc_id] = Session(
             client, dc_id,
             await Auth(client, dc_id, False).create(),
             False, is_media=True
